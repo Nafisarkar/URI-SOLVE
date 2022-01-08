534803f1389d19ec7e586e08a5a29e85c70130b9
Shaon An Nafi <sarkarnafe@gmail.com>
2022-01-08 13:45:13 +0600

    solved added
    problem uri 1547 added

--- URI1547.cpp
+++ URI1547.cpp
@@ -1,27 +1,45 @@
 #include<stdio.h>
-#include"Inout.h"
-
-void funstuff(){
-    int l,srhnum;
-    int loop;
-    scanf("%d %d",&l,&srhnum);
-    int arr[l]={};
-    for(loop=0;loop<l;loop++){
-        scanf("%d",&arr[loop]);
-    }
-    for(loop=0;loop<l;loop++){
-        printf("%d\n",arr[loop]);
-    }
-}
-
-
+#include<stdlib.h>
 int main(){
-    inout();
-    int cas,i;
-    printf("PLEASE ENTER THE CASE\n");
-    scanf("%d",&cas);
-    for(i=0;i<cas;i++){
-        funstuff();
-    }
-    return 0;
+	int c;
+	int a,b,temp;
+	int arr[100]={};
+	int cp[100]={};
+
+	scanf("%d",&c);
+	while(c!=0){
+		scanf("%d %d",&a,&b);
+
+		for(int i=0;i< a; i++){
+			scanf("%d",&temp);
+			arr[i]=abs(temp-b);
+		}
+
+		int b = arr[0];
+		for(int i=0;i< a; i++){
+			if(arr[i]<b){
+				b=arr[i];
+			}
+		}
+		for(int j=0;j< a; j++){
+			cp[j]=arr[j];
+		}
+		for(int i=0;i< a; i++){
+			for(int k=i;k< a; k++){
+				if(cp[i]>cp[k]){
+					int temp = cp[i];
+					cp[i]=cp[k];
+					cp[k]=temp;
+				}
+			}
+		}
+		for(int j=0;j< a; j++){
+			if(arr[j]==cp[0]){
+				printf("%d\n",j+1);
+				break;
+			}
+		}
+		c--;
+	}
+	return 0;
 }
